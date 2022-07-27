0219bbfb33b589aaffa2b44f992da5232f74fbca
alis70929 <60583162+alis70929@users.noreply.github.com>
2022-07-27 12:30:19 +1200

    Got basic show all done

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 ini_set('display_errors', 'On');  // change to 'Off' when live
 
-define ("DB_NAME","editme"); 
-define ("DB_USERNAME","editme");
-define ("DB_PASSWORD","editme"); 
+define ("DB_NAME","alis70929_Lvl3_Quotes"); 
+define ("DB_USERNAME","_alis70929");
+define ("DB_PASSWORD","LhjeBlNpQZacZpDM"); 
 define ("DB_HOST","localhost");
 
 ?>
