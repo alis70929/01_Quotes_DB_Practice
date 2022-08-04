11eff49f88b108e2df27f20fb0ec95d58abe56c4
alis70929 <60583162+alis70929@users.noreply.github.com>
2022-08-05 10:03:12 +1200

    Finished Quick Search

--- content/quick_search.php
+++ content/quick_search.php
@@ -2,63 +2,87 @@
 <?php 
     $quick_find = mysqli_real_escape_string($dbconnect,$_POST['quick_search']);
    
-    $sub_sql = "SELECT * FROM `subject` WHERE `Subject_ID` LIKE '%$subject_to_find%'";
+    $sub_sql = "SELECT * FROM `subject` WHERE `Subject` LIKE '%$quick_find%'";
     $sub_query = mysqli_query($dbconnect,$sub_sql);
     $sub_rs = mysqli_fetch_assoc($sub_query);
 
     $sub_count = mysqli_num_rows($sub_query);
 
-    if ($subject_count > 0)
+    if ($sub_count > 0)
     {
         $subject_ID = $sub_rs['Subject_ID'];
 
     }
     else
     {
-        $subject_ID = -1
+        $subject_ID = -1;
 
     }
     $find_sql = "SELECT * FROM `quotes` 
     JOIN author ON (`author`.`Author_ID`= `quotes`.`Author_ID`)
+    WHERE `author`.`First` LIKE '%$quick_find%' OR
+    `author`.`Middle` LIKE '%$quick_find%' OR
+    `author`.`Last` LIKE '%$quick_find%' OR
+    `quotes`.`Quote` LIKE '%$quick_find%' OR
+    `Subject1_ID` LIKE '$subject_ID' OR
+    `Subject2_ID` LIKE '$subject_ID' OR
+    `Subject3_ID` LIKE '$subject_ID'
     ORDER BY `quotes`.`ID` ASC";
     $find_query = mysqli_query($dbconnect, $find_sql);
     $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query)
 ?>
 
-<h2>All Results</h2>
+<h2>Quick Search - <?php echo $quick_find ?></h2>
 
 <?php
-    do {
+    if($count > 0){
+        do {
 
-        $quote = preg_replace('/[^A-Za-z0-9.,?\s\'\-]/', ' ',$find_rs['Quote'] );
+            $quote = preg_replace('/[^A-Za-z0-9.,?\s\'\-]/', ' ',$find_rs['Quote'] );
 
-        include("functions/get_author.php");
-        ?>
-        <div class="results">
-            <p>
-                <?php echo $quote;?> 
-            </p>
-            
-            <p>
-                <span class=author_tag>
-                    <a href="index.php?page=author&authorID=<?php echo $find_rs['Author_ID']?>">
-                        <?php echo $full_name;?>
-                    </a>
-                </span>
-            </p>
+            include("functions/get_author.php");
+            ?>
+            <div class="results">
+                <p>
+                    <?php echo $quote;?> 
+                </p>
+                
+                <p>
+                    <span class=author_tag>
+                        <a href="index.php?page=author&authorID=<?php echo $find_rs['Author_ID']?>">
+                            <?php echo $full_name;?>
+                        </a>
+                    </span>
+                </p>
 
-            <p>
-                <?php include("functions/show_subjects.php")?>
-            </p>
-        </div>
+                <p>
+                    <?php include("functions/show_subjects.php")?>
+                </p>
+            </div>
 
-        <br />
+            <br />
 
 
-        <?php
+            <?php
 
+        }
+        while($find_rs = mysqli_fetch_assoc($find_query));
     }
-    while($find_rs = mysqli_fetch_assoc($find_query))
+    else
+    {
+        ?>
+        <h2>Oops!</h2>
+            <div class="error">
+
+                Sorry - There are no quotes that match the search term <i><b><?php echo $quick_find?></b></i>
+
+            </div>
+
+            <p>&nbsp;</p>
+                
+        <?php
+    }
 
 
 ?>
