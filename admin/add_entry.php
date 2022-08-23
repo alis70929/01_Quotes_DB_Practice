c49551e58d509fbc9150d5426d0646668675f0f6
alis70929 <60583162+alis70929@users.noreply.github.com>
2022-08-23 13:28:59 +1200

    checkpoint 5 Add entris complete

--- admin/add_entry.php
+++ admin/add_entry.php
@@ -26,7 +26,10 @@
         if($_SERVER["REQUEST_METHOD"] == "POST")
         {
             $quote = mysqli_real_escape_string($dbconnect, $_POST['quote']);
-
+            $notes = mysqli_real_escape_string($dbconnect, $_POST['notes']);
+            $tag_1 = mysqli_real_escape_string($dbconnect, $_POST['Subject_1']);
+            $tag_2 = mysqli_real_escape_string($dbconnect, $_POST['Subject_2']);
+            $tag_3 = mysqli_real_escape_string($dbconnect, $_POST['Subject_3']);
             if($quote == "Please Type the quote here" ){
                 $has_errors = "yes";
                 $quote_error = "error-text";
@@ -39,12 +42,31 @@
                 $tag_1_error = "error-text";
                 $tag_1_field = "tag-error";
             }
+            if($has_errors != "yes"){
+                $subjectID_1 = get_ID($dbconnect,'subject', 'Subject_ID',"Subject",$tag_1);
+                $subjectID_2 = get_ID($dbconnect,'subject', 'Subject_ID',"Subject",$tag_2);
+                $subjectID_3 = get_ID($dbconnect,'subject', 'Subject_ID',"Subject",$tag_3);
+
+                $addentry_sql = "INSERT INTO `quotes` (`ID`, `Author_ID`, `Quote`, `Notes`, `Subject1_ID`, `Subject2_ID`, `Subject3_ID`) 
+                VALUES (NULL, '$author_ID', '$quote', '$notes', '$subjectID_1', '$subjectID_2', '$subjectID_3')";
+                $addentry_query = mysqli_query($dbconnect,$addentry_sql);
+
+                $get_quote_sql = "SELECT * FROM `quotes` WHERE `Quote`= '$quote'";
+                $get_quote_query = mysqli_query($dbconnect,$get_quote_sql);
+                $get_quote_rs = mysqli_fetch_assoc($get_quote_query);
+
+                $quote_ID = $get_quote_rs['ID'];
+                $_SESSION['Quote_Success'] = $quote_ID;
+                echo($quote_ID);
+                echo($addentry_sql);
+                header("Location: index.php?page=quote_success");
+            }
         }
 
     }
     else{
         $login_error = "please login to acces this page";
-        header("Location: index.php?page=../admin/login&error=$login_error&hash=$hash");
+        header("Location: index.php?page=../admin/login&error=$login_error");
     }
 
 ?>
@@ -77,6 +99,23 @@ enctype="multipart/form-data">
         name="Subject_1" placeholder="Subject 1(Start Typing)...">
 
     </div>
+
+    <br /><br />
+
+    <div class="autocomplete">
+        <input id="subject2" type="text"
+        name="Subject_2" placeholder="Subject 2(Start Typing)...">
+
+    </div>
+
+    <br /><br />
+
+    <div class="autocomplete">
+        <input id="subject3" type="text"
+        name="Subject_3" placeholder="Subject 3(Start Typing)...">
+
+    </div>
+
     <p>
         <input type="submit" value = "Submit"/>
     </p>
@@ -88,5 +127,6 @@ enctype="multipart/form-data">
 
 var all_tags = <?php print("$all_subjects"); ?>;
 autocomplete(document.getElementById("subject1"), all_tags);
-
+autocomplete(document.getElementById("subject2"), all_tags);
+autocomplete(document.getElementById("subject3"), all_tags);
 </script>
